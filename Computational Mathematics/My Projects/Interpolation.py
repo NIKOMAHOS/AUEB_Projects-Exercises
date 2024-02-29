1d56539752514afc453e8c06cfd3e7ae2206b6d7
NIKOMAHOS <mitsakisnick@gmail.com>
2024-02-29 10:41:42 +0200

    Update Interpolation.py

--- Computational Mathematics/My Projects/Interpolation.py
+++ Computational Mathematics/My Projects/Interpolation.py
@@ -1,128 +1,62 @@
-import numpy
-import math
-import matplotlib.pyplot as PLUDecomposition
-import copy
-from LinearAlgebra import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-def ForwardDif(f):
-    x = copy.deepcopy(f)
-    result = []
+#define Runge function
+def f(x):
+    return 1.0 / ( 1 + 25*x**2 )
 
-    for i in range(1, len(f)):
-        for j in range(len(f) - i):
-            x[j] = x[j + 1] - x[j]
+#Interpolating f using
+#20 points equally spaced points between -1 and 1
+x = np.linspace( -1, 1, 20 )
+y = f(x)
 
-        print (x[:-i])
-        result.append(copy.deepcopy(x[:-i]))
-    
-    return result
+def interpolation( x, y, z ):
+    #x,y,z all must be 1d array
 
-def BackwardDif(f):
-    x = copy.deepcopy(f)
-    result = []
+    #x and y are the interpolated data points (x,y)
+    #z vector where we want the value of polynomial
 
-    for i in range(len(f) - 1):
-        for j in range(len(f) - 1,  i, -1):
-            x[j] = x[j] - x[j - 1]
+    val = np.zeros( (z.shape[0],1) )
 
-        result.append(copy.deepcopy(x[i + 1:]))
-    
-    return result
+    #for each point in z
+    for k in range( z.shape[0] ):
+        #calc val[k]
+        for i in range( x.shape[0] ):
+            #Initialize the product
+            prod = 1
+            #iterate for all x_j
+            for j in range( x.shape[0] ):
+                if i != j:
+                    prod *= ( z[k] - x[j] )
+                    prod /= ( x[i] - x[j] )
+            val[k] += prod * y[i]
 
-def DividedDif(f, x):
-    result = []
-    y = copy.deepcopy(f)
+    return val
 
-    for i in range(1, len(f)):
-        for j in range(len(f) - i):
-            y[j] = (y[j + 1] - y[j]) / (x[j + i] - x[j])
+#use linspace as stated in description
+z = np.linspace( -1, 1, 39 )
 
+#Calculate 20 chebyshev points
+inp = np.arange(1,21).reshape( (-1,1) )
+cheb_x = np.cos( (2*inp - 1) * np.pi / ( 2*inp.shape[0] ) )
+cheb_y = f(cheb_x)
 
-        result.append(copy.deepcopy(y[:-i]))
-    return result
+print( cheb_x )
 
-def NewtonForward(f, x, theta):
+#run interpolation with uniform partition
+uniform_estimated = interpolation( x, y, z )
 
-    diff = ForwardDif(f)
-    result = f[0]
+#run interpolation with chebyshev partition
+cheb_estimated = interpolation( cheb_x, cheb_y, z )
 
-    for i in range(1, len(diff) + 1):
-        temp = 1
-        for j in range(i):
-            temp *= (theta - j) / (j + 1)
-        
-        result += diff[i - 1][0] * temp
-    
-    return result
+plt.plot( z, f(z), label='true values' )
+plt.plot( x, y, '*', label='interpolated points' )
+plt.plot( cheb_x, cheb_y, 'b+', label='chebyshev points' )
 
-def NewtonBackward(f, x, theta):
+plt.plot( z, uniform_estimated, '--', label='uniform estimated values' )
+plt.plot( z, cheb_estimated, 'y-', label='cheb estimated values' )
 
-    diff = BackwardDif(f)
-    result = f[-1]
-
-    for i in range(1, len(diff) + 1):
-        temp = 1
-        for j in range(i):
-            temp *= (theta + j) / (j + 1)
-        
-        result += diff[i - 1][-1] * temp
-    
-    return result
-
-def Chebyshev(a, b, n):
-
-
-    return [(b + a) / 2 + (b - a) / 2 * math.cos((2 * i - 1) * math.pi / (2 * n)) for i in range(1, n + 1)]
-
-def NewtonDivided(f, x, x_star):
-
-    diff = DividedDif(f, x)
-    result = f[0]
-
-    for i in range(1, len(diff) + 1):
-        temp = 1
-        for j in range(i):
-            temp *= x_star - x[j]
-
-        result += temp * diff[i - 1][0]
-
-    return result
-
-def Lagrange(f, x, x_star):
-
-    result = 0
-    for i in range(len(f)):
-        temp = 1
-        for j in range(len(f)):
-
-            if j == i: continue
-
-            temp *= (x_star - x[j]) / (x[i] - x[j])
-
-        result += temp * f[i]
-    return result
-
-def SplinesNatural(f, h):
-    A = []
-
-    for i in range(len(f)):
-        if i == 0:
-            A.append([1 if j == 0 else 0 for j in range(len(f) + 1)])
-        elif i == len(f) - 1:
-            A.append([1 if j == len(f) - 1 else 0 for j in range(len(f) + 1)])
-        else:
-            row = numpy.zeros(len(f) + 1)
-            row[i - 1] = h[i]
-            row[i] = 2 * (h[i - 1] + h[i])
-            row[i + 1] = h[i]
-            row[-1] = 3 * (f[i + 1] - f[i]) / h[i] - 3 * (f[i] - f[i - 1])/ h[i - 1]
-            A.append(copy.deepcopy(row))
-
-    x = gauss(A)
-
-    b = [(f[i + 1] - f[i]) / h[i] - h[i] * (2 * x[i] + x[i + 1]) / 3 for i in range(len(f) - 1)]
-    d = [(x[i + 1] - x[i]) / h[i] / 3 for i in range(len(f) - 1)]
-
-    return f[:-1], b, x[:-1], d
-
-            
+plt.axis( [-1.5,1.5,-1.5,7] )
+plt.grid()
+plt.legend()
+plt.show()
